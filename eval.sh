e718f29a589a74591b2d6c3d1affa6398751d1c4
xkliu <chthollylxk@mail.ustc.edu.cn>
2025-05-06 11:10:11 +0800

    remove no use eavl.

--- eval.sh
+++
@@ -1,6 +0,0 @@
-export CUDA_VISIBLE_DEVICES=2
-python code/eval.py \
-    --dataset_name MMLU \
-    --model_name Llama \
-    --file_path /data/xkliu/LLMs/DocFixQA/result/MMLU/turn1_rag/ \
-    --answer_key llm_response
